4138c349661799d2263ddf5424a7d4335515b3c4
AbderrahimO <43134874+AbderrahimO@users.noreply.github.com>
2019-06-18 22:45:17 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,52 @@
+
+
+<footer class="footer-distributed" style="position: relative;
+    bottom: 0">
+	<div class="footer-left">
+		<h1>ALUME</h1>
+		<p class="footer-links">
+			<a href="index.php">ACCUEIL</a>
+			·
+			<a href="article.php">ARTICLE</a>
+			·
+			<a href="espace_membre/connexion.php">ESPACES MEMBRE</a>
+			·
+			<a href="contact.php">CONTACT</a>
+			.
+			<a href="panier/index.php">PANIER</a>
+
+		</p>
+		<p class="footer-company-name">Alume &copy; 2019</p>
+	</div>
+	<div class="footer-center">
+		<div>
+			<i class="fa fa-map-marker"></i>
+			<p><span>2, Impasse des 2 cousins</span> Paris, France</p>
+		</div>
+		<div>
+			<i class="fa fa-phone"></i>
+			<p>06 51 10 99 33 </p>
+		</div>
+		<div>
+			<i class="fa fa-envelope"></i>
+			<p><a href="#">contact@alume.com</a></p>
+		</div>
+	</div>
+
+	<div class="footer-right">
+
+		<p class="footer-company-about">
+			<span>A propos de l'entreprise</span>
+Plus de 20 ans d'expertise dans la construction.
+		</p>
+
+		<div class="footer-icons">
+			<a href="#"><i class="fa fa-facebook"></i></a>
+			<a href="#"><i class="fa fa-twitter"></i></a>
+			<a href="#"><i class="fa fa-linkedin"></i></a>
+			<a href="#"><i class="fa fa-github"></i></a>
+		</div>
+	</div>
+</footer>
+
+
